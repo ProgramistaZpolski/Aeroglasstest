09e564366ba5d2117b1cb54f2b2342da35b138b7
ProgramistaZpolski <37271169+ProgramistaZpolski@users.noreply.github.com>
2020-02-04 09:51:57 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CentrumAkcji
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            // Use e.g. Color.FromArgb(128, Color.Lime) for a 50% opacity green tint.
+            WindowUtils.EnableAcrylic(this, Color.FromArgb(0, Color.Lime));
+
+            base.OnHandleCreated(e);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Transparent);
+        }
+    }
+}
